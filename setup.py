64bac1898f24667f943b13bd01401d11f831ed3c
Jani Kajala <kajala@gmail.com>
2025-10-24 17:30:30 -0500

    Release 0.4.9

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ packages = find_packages(exclude=["*.venv", "*.venv.*", "venv.*", "venv"])
 
 setup(
     name="django-jstocks",
-    version="0.4.8",
+    version="0.4.9",
     author="Jani Kajala",
     author_email="kajala@gmail.com",
     packages=packages,
